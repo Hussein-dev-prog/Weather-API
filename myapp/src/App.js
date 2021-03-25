1a7b85530ceae93a8cc27bc92201083aa41f4363
Hussein-dev-prog <hussein.aref.csci@gmail.com>
2021-03-25 14:40:01 +0200

    Weather API: HTML/CSS

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hussein&Ziad        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="WeatherAPI">
+      <header className="SearchContainer">
+        <input type="text" placeholder="Type in a city name"></input>
+        <button type="submit">FIND WEATHER</button>
       </header>
+      <main className="MainApp">
+        <div className="pic1">
+          <img src="https://placekitten.com/150/150" alt="kitten"></img>
+          <div id="below-pic">
+            <p id="p1">Hello </p>
+            <p id="p1">Hello 1</p>
+            <p id="p1">Hello 2</p>
+          </div>
+        </div>
+
+        <div className="temp24" >
+          <div><p id="Time">1</p><img src="https://placekitten.com/50/50" alt="kitten"></img><p id="Deg">11</p></div>
+
+          <div><p id="Time">2</p><img src="https://placekitten.com/50/50" alt="kitten"></img><p id="Deg">22</p></div>
+
+          <div><p id="Time"> 3</p><img src="https://placekitten.com/50/50" alt="kitten"></img><p id="Deg">33</p></div>
+
+          <div><p id="Time"> 4</p><img src="https://placekitten.com/50/50" alt="kitten"></img><p id="Deg">44</p></div>
+
+          <div><p id="Time">5</p><img src="https://placekitten.com/50/50" alt="kitten"></img><p id="Deg">55</p></div>
+
+          <div><p id="Time">6</p><img src="https://placekitten.com/50/50" alt="kitten"></img><p id="Deg">66</p></div>
+
+          <div><p id="Time">7</p><img src="https://placekitten.com/50/50" alt="kitten"></img><p id="Deg">77</p></div>
+        </div>
+      </main>
     </div>
   );
 }
